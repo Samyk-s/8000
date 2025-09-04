8947de12ed37add41292c0ede64ac04ec1c4b8d8
dammar093 <dammarrana093@gmail.com>
2025-09-04 18:29:37 +0545

    clean header

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,12 @@
 
 import { SidebarProvider } from "@/components/Layouts/sidebar/sidebar-context";
 import { store } from "@/redux-store/store/store";
-import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <ThemeProvider defaultTheme="light" attribute="class">
-        <SidebarProvider>{children}</SidebarProvider>
-      </ThemeProvider>
+      <SidebarProvider>{children}</SidebarProvider>
     </Provider>
   );
 }
