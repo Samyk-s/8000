e9cc9f429b45a88ebaad62a8b07073c7a5c1ca94
dammar093 <dammarrana093@gmail.com>
2025-09-14 15:54:56 +0545

    fixed loader

--- src/components/adminComponents/pages-components/loader/loader.tsx
+++ src/components/adminComponents/pages-components/loader/loader.tsx
@@ -1,4 +1,5 @@
 import { LoaderIcon } from "@/components/icons/icnos";
+import { Spin } from "antd";
 import React from "react";
 
 const Loader = () => {
@@ -6,8 +7,7 @@ const Loader = () => {
     <div className="h-screen bg-gray-50 p-6">
       <div className="light h-full rounded-lg bg-white p-8 shadow-sm">
         <div className="flex h-full items-center justify-center">
-          <LoaderIcon />
-          <span className="ml-2 text-gray-600">Loading...</span>
+          <Spin />
         </div>
       </div>
     </div>
