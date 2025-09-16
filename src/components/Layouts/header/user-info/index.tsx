52c4d36cfa1fcac39dcc8874efc30c7a3e38a131
dammar093 <dammarrana093@gmail.com>
2025-09-16 13:20:38 +0545

    added account setting

--- src/components/Layouts/header/user-info/index.tsx
+++ src/components/Layouts/header/user-info/index.tsx
@@ -10,10 +10,13 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { LogOutIcon, SettingsIcon, UserIcon } from "./icons";
+import { LogOutIcon, SettingsIcon } from "./icons";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux-store/store/store";
 
 export function UserInfo() {
   const [isOpen, setIsOpen] = useState(false);
+  const { item } = useSelector((state: RootState) => state.setting);
 
   const USER = {
     name: "John Smith",
@@ -28,25 +31,13 @@ export function UserInfo() {
 
         <figure className="flex items-center gap-3">
           <Image
-            src={USER.img}
-            className="size-12"
-            alt={`Avatar of ${USER.name}`}
+            src={`${item?.logo?.url || "/images/broken/broken.png"}`}
+            className="size-12 rounded-full border-2 border-blue-600"
+            alt={`Avatar of ${item?.title}`}
             role="presentation"
             width={200}
             height={200}
           />
-          <figcaption className="flex items-center gap-1 font-medium text-dark dark:text-dark-6 max-[1024px]:sr-only">
-            <span>{USER.name}</span>
-
-            <ChevronUpIcon
-              aria-hidden
-              className={cn(
-                "rotate-180 transition-transform",
-                isOpen && "rotate-0",
-              )}
-              strokeWidth={1.5}
-            />
-          </figcaption>
         </figure>
       </DropdownTrigger>
 
@@ -54,42 +45,9 @@ export function UserInfo() {
         className="border border-stroke bg-white shadow-md dark:border-dark-3 dark:bg-gray-dark min-[230px]:min-w-[17.5rem]"
         align="end"
       >
-        <h2 className="sr-only">User information</h2>
-
-        <figure className="flex items-center gap-2.5 px-5 py-3.5">
-          <Image
-            src={USER.img}
-            className="size-12"
-            alt={`Avatar for ${USER.name}`}
-            role="presentation"
-            width={200}
-            height={200}
-          />
-
-          <figcaption className="space-y-1 text-base font-medium">
-            <div className="mb-2 leading-none text-dark dark:text-white">
-              {USER.name}
-            </div>
-
-            <div className="leading-none text-gray-6">{USER.email}</div>
-          </figcaption>
-        </figure>
-
-        <hr className="border-[#E8E8E8] dark:border-dark-3" />
-
         <div className="p-2 text-base text-[#4B5563] dark:text-dark-6 [&>*]:cursor-pointer">
           <Link
-            href={"/profile"}
-            onClick={() => setIsOpen(false)}
-            className="flex w-full items-center gap-2.5 rounded-lg px-2.5 py-[9px] hover:bg-gray-2 hover:text-dark dark:hover:bg-dark-3 dark:hover:text-white"
-          >
-            <UserIcon />
-
-            <span className="mr-auto text-base font-medium">View profile</span>
-          </Link>
-
-          <Link
-            href={"/pages/settings"}
+            href={"/admin/me"}
             onClick={() => setIsOpen(false)}
             className="flex w-full items-center gap-2.5 rounded-lg px-2.5 py-[9px] hover:bg-gray-2 hover:text-dark dark:hover:bg-dark-3 dark:hover:text-white"
           >
