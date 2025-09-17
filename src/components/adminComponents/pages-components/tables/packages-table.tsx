f5c9bd320635aa078c80a429ba9e801365c81027
dammar093 <dammarrana093@gmail.com>
2025-09-17 13:02:22 +0545

    added package hook

--- src/components/adminComponents/pages-components/tables/packages-table.tsx
+++ src/components/adminComponents/pages-components/tables/packages-table.tsx
@@ -1,5 +1,12 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Entry from "../../entry/entry";
+import Search from "../../search/search";
+import Pagination from "../../pagination/pagination";
+import ToggleButton from "../../toggle-button/toggle-button";
+import Loader from "../loader/loader";
 import {
   EditIcon,
   ItinerayIcon,
@@ -8,70 +15,26 @@ import {
   ReviewIcon,
   SeoIcon,
 } from "@/components/icons/icnos";
-import { PackageItem } from "@/types/package";
-import Image from "next/image";
-import Link from "next/link";
-import Entry from "../../entry/entry";
-import Search from "../../search/search";
-import Pagination from "../../pagination/pagination";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/redux-store/store/store";
-import {
-  fetchPackages,
-  searchPackages,
-  togglePackageStatus,
-} from "@/redux-store/slices/packageSlice";
-import ToggleButton from "../../toggle-button/toggle-button";
 import { PlusIcon } from "@/assets/icons";
-import Loader from "../loader/loader";
+import { usePackage } from "@/hooks/package/usePackage";
 import { message } from "antd";
 
 const PackageTable: React.FC = () => {
-  const [limit, setLimit] = useState(10);
-  const { items, loading, error, meta } = useSelector(
-    (state: RootState) => state?.packges,
-  );
-  const [page, setPage] = useState(meta?.currentPage);
-  const dispatch = useDispatch<AppDispatch>();
-  const [search, setSearch] = useState("");
-  const debounceRef = useRef<NodeJS.Timeout | null>(null);
+  const {
+    items,
+    loading,
+    error,
+    meta,
+    limit,
+    search,
+    handleSearch,
+    handleToggleStatus,
+    changePage,
+    changeLimit,
+  } = usePackage();
 
-  // call api for getting packages
-  useEffect(() => {
-    dispatch(fetchPackages({ page, limit }));
-  }, [dispatch, page, limit]);
-
-  // search itinerary
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setSearch(value);
-
-    // Clear previous timeout
-    if (debounceRef.current) {
-      clearTimeout(debounceRef.current);
-    }
-
-    // Set new timeout
-    debounceRef.current = setTimeout(() => {
-      dispatch(
-        searchPackages({
-          params: {
-            limit,
-            page,
-            search,
-          },
-        }),
-      );
-    }, 300);
-  };
-
-  if (loading) {
-    return <Loader />;
-  }
-
-  if (error) {
-    message.error(error);
-  }
+  if (loading) return <Loader />;
+  if (error) message.error(error);
 
   return (
     <div className="min-h-screen p-1">
@@ -86,16 +49,17 @@ const PackageTable: React.FC = () => {
               <span>Create</span>
             </Link>
           </div>
+
           <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
             <Entry
-              onChange={(value) => setLimit(Number(value))}
               value={limit}
+              onChange={(val) => changeLimit(Number(val))}
               total={meta?.totalItems}
             />
             <Search
               placeholder="Search package..."
               search={search}
-              onChange={handleSearch}
+              onChange={(e) => handleSearch(e.target.value)}
             />
           </div>
 
@@ -126,24 +90,25 @@ const PackageTable: React.FC = () => {
                   </th>
                 </tr>
               </thead>
+
               <tbody className="divide-y divide-gray-200 bg-white">
-                {items && items?.length > 0 ? (
-                  items?.map((item: PackageItem, index) => (
-                    <tr key={item?.id} className="hover:bg-gray-50">
+                {items && items.length > 0 ? (
+                  items.map((item, index) => (
+                    <tr key={item.id} className="hover:bg-gray-50">
                       <td className="whitespace-nowrap px-6 py-4 text-base text-gray-900">
                         {index + 1}
                       </td>
                       <td className="whitespace-nowrap px-6 py-4">
                         <Link
-                          href={`${item?.image?.url || "/images/broken/broken.png"}`}
+                          href={item.image?.url || "/images/broken/broken.png"}
                           target="_blank"
                         >
                           <div className="h-20 w-30 text-base font-medium text-gray-900">
                             <Image
                               src={
-                                item?.image?.url || "/images/broken/broken.png"
+                                item.image?.url || "/images/broken/broken.png"
                               }
-                              alt={item?.title}
+                              alt={item.title}
                               width={1080}
                               height={720}
                               className="aspect-video"
@@ -152,68 +117,58 @@ const PackageTable: React.FC = () => {
                         </Link>
                       </td>
                       <td className="whitespace-nowrap px-6 py-4 text-base text-gray-900">
-                        {item?.title}
+                        {item.title}
                       </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        <div className="text-base text-gray-600">
-                          {item?.altitude}
-                        </div>
+                      <td className="whitespace-nowrap px-6 py-4 text-base text-gray-600">
+                        {item.altitude}
                       </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        <div className="text-base text-gray-500">
-                          {item?.grade}
-                        </div>
+                      <td className="whitespace-nowrap px-6 py-4 text-base text-gray-500">
+                        {item.grade}
                       </td>
-
                       <td className="whitespace-nowrap px-6 py-4 text-base font-medium">
                         <div className="flex space-x-2">
                           <Link
-                            href={`/admin/packages/${item?.id}/itinerary`}
+                            href={`/admin/packages/${item.id}/itinerary`}
                             title="Itinerary"
                           >
                             <ItinerayIcon />
                           </Link>
-
                           <Link
-                            href={`/admin/packages/${item?.id}/gallery`}
+                            href={`/admin/packages/${item.id}/gallery`}
                             title="Gallery"
                           >
                             <GalleryIcon />
                           </Link>
                           <Link
-                            href={`/admin/packages/${item?.id}/departure`}
+                            href={`/admin/packages/${item.id}/departure`}
                             title="Departure"
                           >
                             <DepartureIcon />
                           </Link>
-
                           <Link
-                            href={`/admin/packages/${item?.id}/review`}
+                            href={`/admin/packages/${item.id}/review`}
                             title="Review"
                           >
                             <ReviewIcon />
                           </Link>
-
                           <Link
-                            href={`/admin/packages/${item?.id}/seo`}
+                            href={`/admin/packages/${item.id}/seo`}
                             title="SEO"
                           >
                             <SeoIcon />
                           </Link>
-
                           <Link
-                            href={`/admin/packages/${item?.id}`}
+                            href={`/admin/packages/${item.id}`}
                             title="Edit Package"
                           >
                             <EditIcon />
                           </Link>
+
                           <ToggleButton
-                            onChange={() => {
-                              dispatch(togglePackageStatus(item?.id));
-                            }}
-                            checked={item?.status === 1 ? true : false}
+                            onChange={() => handleToggleStatus(item.id)}
+                            checked={item.status === 1}
                             title={
-                              item?.status === 1
+                              item.status === 1
                                 ? "Deactive Package"
                                 : "Active Package"
                             }
@@ -228,20 +183,20 @@ const PackageTable: React.FC = () => {
                       colSpan={7}
                       className="px-6 py-8 text-center text-base text-gray-500"
                     >
-                      No packages found .
+                      No packages found.
                     </td>
                   </tr>
                 )}
               </tbody>
             </table>
           </div>
-          {/* Pagination */}
+
           <Pagination
             currentPage={meta?.currentPage}
             totalPages={meta?.totalPages}
             itemsPerPage={limit}
             totalItems={meta?.totalItems}
-            onPageChange={(page) => setPage(page)}
+            onPageChange={changePage}
           />
         </div>
       </div>
